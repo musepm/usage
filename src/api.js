1a9612e1b8a270ebedba55c927c7797372c10782
Jason Livesay <ithkuil@gmail.com>
2015-06-23 23:21:19 +0000

    revise api

--- src/api.js
+++ src/api.js
@@ -1,17 +1,22 @@
-var koa = require('koa');
-require('koa-router');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    eventLog = require('./eventlog'),
+    app = express(),
+    cn = require('./default.conf.json').dbconn,
+    eventLog = require('./eventlog').createLog(cn);
 
-var cn = require('../../default.conf.json').dbconn;
+app.use(bodyParser.json());
 
-var eventLog = require ('../eventlog').createLog(cn);
-
-var app = koa(); 
-
-var jsonBody = require('koa-json-body')({ limit: '10kb' });
- 
-app.post('/users', jsonBody, function *() {
-  console.log(this.request.body);
-  eventLog.addEvent(this.request.body);
+app.post('/events', (req, res) => {
+  try {
+    eventLog.addEvent(req.body);
+    res.json({status:'ok'});
+  } catch (e) {
+    res.json({status:'error', error: e.message});
+  }
 });
 
-app.listen(3000);
+var server = app.listen(3000, => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port); });
